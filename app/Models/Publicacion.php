2381f2af63e15ec53cab119eed433e997c11688f
digitalhouse <dxe15104@miauj.com>
2018-07-20 15:24:20 -0300

    modificacion rutas con auth

--- app/Models/Publicacion.php
+++ app/Models/Publicacion.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Publicacion;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+
 
 class Publicacion extends Model
 {
     protected $table = 'publicaciones';
+
+public function publicacion() {
+  return $this->hasMany(Publicacion::class, 'id_usuario', 'id');
+  }
 }
